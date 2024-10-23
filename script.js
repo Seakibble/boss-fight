5afcd0edf6adc26eaac72c1a109f30849104da12
Seakibble <max.leeming@gmail.com>
2024-10-23 17:13:17 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,110 @@
+let $healthBars = document.getElementById('healthBars')
+let $input = document.getElementById('input')
+
+$input.addEventListener('keyup', e => {
+    if (e.key === 'Enter') {
+        let data = $input.value.split(' ')
+        
+        switch (data[0]) {
+            case 'dam':
+                damage(data[1], data[2]);
+                break;
+            case 'heal':
+                heal(data[1], parseInt(data[2]));
+                break;
+            case 'temp':
+                giveTempHP(data[1], parseInt(data[2]));
+                break;
+            case 'add':
+                addHealthBar(data[1], data[2], parseInt(data[3]));
+                break;
+        }
+        
+    }
+})
+
+let bosses = {}
+
+function addHealthBar(id, name, hp) {
+    name = name.replaceAll('_',' ')
+    let $boss = document.createElement('div')
+    $boss.classList.add('bossHealthBar')
+    $boss.innerHTML = `
+<h2 class="name">${name}</h2>
+<h3 class="damageText" data-damage=0></h3>
+<h3 class="tempText"></h3>
+<div class='healthBar'>
+    <div class="hp"></div>
+    <div class="hpGhost"></div>
+</div>`
+    $healthBars.appendChild($boss)
+
+    bosses[id] = {
+        hp: hp,
+        max: hp,
+        temp: 0,
+        el: $boss
+    }
+
+    updateBar(id)
+}
+
+function damage(name, amount) {
+    let boss = bosses[name]
+    boss.temp -= amount
+    if (boss.temp < 0) {
+        boss.hp += boss.temp
+        boss.temp = 0
+    }
+    if (boss.hp < 0) boss.hp = 0
+
+    let damage = boss.el.querySelector('.damageText').dataset.damage
+    damage = parseInt(amount) + parseInt(damage)
+    boss.el.querySelector('.damageText').innerHTML = damage
+    boss.el.querySelector('.damageText').dataset.damage = damage
+
+    updateBar(name)
+}
+
+function giveTempHP(name, amount) {
+    let boss = bosses[name]
+    boss.temp = amount
+
+    updateBar(name)
+}
+
+function heal(name, amount) {
+    let boss = bosses[name]
+    boss.hp += amount
+    if (boss.hp > boss.max) boss.hp = boss.max
+    
+    boss.el.classList.add('healing')
+    setTimeout(()=> {
+        boss.el.classList.remove('healing')
+    }, 4000)
+
+    boss.el.querySelector('.damageText').dataset.damage = 0
+    boss.el.querySelector('.damageText').innerHTML = ''
+    
+    updateBar(name)
+}
+
+function updateBar(name) {
+    let boss = bosses[name]
+    let health = (boss.hp/boss.max*100) + '%'
+    boss.el.querySelector('.hp').style.width = health
+    boss.el.querySelector('.hpGhost').style.width = health
+
+    if (boss.temp <= 0) {
+        boss.el.classList.remove('shield')
+    } else {
+        boss.el.classList.add('shield')
+        boss.el.querySelector('.tempText').innerHTML = boss.temp
+        boss.el.querySelector('.tempText').classList.add('ping')
+        setTimeout(()=> {
+            boss.el.querySelector('.tempText').classList.remove('ping')
+        }, 750)
+    }
+}
+
+addHealthBar('rem', 'Remigius of Tenmir', 60)
